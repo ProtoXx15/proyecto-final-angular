9e6547f4532ef2cad67cb120dd941302bd175ad8
Daniel <danigondiaz@gmail.com>
2024-03-10 21:54:23 +0100

    A falta de arreglar el home

--- src/app/pages/recetas/recetas.component.ts
+++ src/app/pages/recetas/recetas.component.ts
@@ -1,25 +1,30 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-interface Receta {
-  id: number;
-  nombre: string;
-  descripcion: string;
-  ingredientes: string[];
-  preparacion: string[];
-}
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
+  selector: 'recetas',
   templateUrl: './recetas.component.html',
-  styleUrls: ['./recetas.component.css']
+  styleUrls: ['./recetas.component.css'],
+  standalone: true,
+  imports: [FormsModule],
+
 })
 export class recetasComponent {
   searchTerm: string = '';
-  recetas: Receta[] = [];
+  recetas: any[] = [];
   loading: boolean = true;
+  nuevaRecetaVisible: boolean = false;
+  nuevaReceta: any = {
+    id: 0,
+    nombre: '',
+    descripcion: '',
+    lugar_de_creacion: '',
+    precio: ''
+  };
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.fetchRecetas();
@@ -33,12 +38,30 @@ export class recetasComponent {
       });
   }
 
-  search(term: string) {
-    this.loading = true;
-    this.http.get<any>('http://localhost:3000/recetas?q=' + term)
-      .subscribe(data => {
-        this.recetas = data.recetas;
-        this.loading = false;
-      });
+  eliminarReceta(id: number) {
+    this.http.delete(`http://localhost:3000/recetas/${id}`).subscribe(() => {
+      this.recetas = this.recetas.filter(receta => receta.id !== id)
+    }) 
+  }
+
+  agregarNuevaReceta() {
+    // Aquí podrías enviar la nueva receta al servidor si es necesario
+    // Por simplicidad, solo la agregaremos localmente
+    this.nuevaReceta.id = this.recetas.length + 1;
+    this.nuevaReceta.precio = '$ ' + this.nuevaReceta.precio;
+    // this.recetas.push({...this.nuevaReceta});
+    this.http.post('http://localhost:3000/recetas', this.nuevaReceta).subscribe(
+      receta => {
+        this.recetas.push(receta);
+        this.nuevaReceta = {
+          id: 0,
+          nombre: '',
+          descripcion: '',
+          lugar_de_creacion: '',
+          precio: ''
+        };
+        this.nuevaRecetaVisible = false;
+      }
+    )
   }
 }
