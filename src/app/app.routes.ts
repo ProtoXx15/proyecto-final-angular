7ddad30bf100134829d467de896a24ae602e56d7
Daniel <danigondiaz@gmail.com>
2024-03-10 04:01:56 +0100

    camera-ts-routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ClimaComponent } from './pages/clima/clima.component';
 import { HomeComponent } from './pages/home/home.component';
 import { GithubComponent } from './pages/github/github.component';
+import { CameraComponent } from './pages/camera/camera.component';
 import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
 export const routes: Routes = [
@@ -9,5 +10,6 @@ export const routes: Routes = [
   { path: 'clima', component: ClimaComponent },
   { path: 'github', component: GithubComponent },
   { path: 'server', component: UsuariosComponent },
+  { path: 'camera', component: CameraComponent }, 
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
--- src/app/pages/camera/camera.component.ts
+++ src/app/pages/camera/camera.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-camera',
-  standalone: true,
-  imports: [],
   templateUrl: './camera.component.html',
-  styleUrl: './camera.component.css'
+  styleUrls: ['./camera.component.css']
 })
-export class CameraComponent {
+export class CameraComponent implements OnDestroy {
+  cameraStream: MediaStream | undefined;
+  @ViewChild('cameraFeed') cameraFeed: ElementRef<HTMLVideoElement> | undefined;
 
+  openCamera() {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(stream => {
+        if (this.cameraFeed) {
+          this.cameraFeed.nativeElement.srcObject = stream;
+        }
+        this.cameraStream = stream;
+      })
+      .catch(error => {
+        console.error('Error opening camera:', error);
+      });
+  }
+
+  closeCamera() {
+    if (this.cameraStream) {
+      this.cameraStream.getTracks().forEach(track => {
+        track.stop();
+      });
+      this.cameraStream = undefined;
+    }
+  }
+
+  ngOnDestroy() {
+    this.closeCamera();
+  }
+
+  ngAfterViewInit() {
+    // Llamamos a openCamera() aquí para asegurarnos de que el elemento de video esté disponible.
+    this.openCamera();
+  }
 }
+
