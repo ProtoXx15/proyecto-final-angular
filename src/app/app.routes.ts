2ada8fb6455960f4b56a65367e829e530c729c80
Daniel <danigondiaz@gmail.com>
2024-03-10 13:45:07 +0100

    calculadora wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { GithubComponent } from './pages/github/github.component';
 import { CameraComponent } from './pages/camera/camera.component';
 import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { CalculadoraComponent } from './pages/calculadora/calculadora.component';
 import { DogsComponent } from './pages/dogs/dogs.component';
 
 export const routes: Routes = [
@@ -12,6 +13,7 @@ export const routes: Routes = [
   { path: 'github', component: GithubComponent },
   { path: 'dogs', component: DogsComponent },
   { path: 'server', component: UsuariosComponent },
-  { path: 'camera', component: CameraComponent }, 
+  { path: 'calculadora', component: CalculadoraComponent },
+  { path: 'camera', component: CameraComponent },
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
---
+++ src/app/pages/adivinanza/adivinanza.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adivinanza',
+  standalone: true,
+  imports: [],
+  templateUrl: './adivinanza.component.html',
+  styleUrl: './adivinanza.component.css'
+})
+export class AdivinanzaComponent {
+
+}
