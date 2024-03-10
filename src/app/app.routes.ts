db653c0441b96c6ddc09a62355deb1342702f36b
Daniel <danigondiaz@gmail.com>
2024-03-10 14:54:35 +0100

    añadidos los componentes calculadora y adivinanza, a falta de css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,17 @@ import { ClimaComponent } from './pages/clima/clima.component';
 import { HomeComponent } from './pages/home/home.component';
 import { GithubComponent } from './pages/github/github.component';
 import { CameraComponent } from './pages/camera/camera.component';
-import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 import { CalculadoraComponent } from './pages/calculadora/calculadora.component';
 import { DogsComponent } from './pages/dogs/dogs.component';
+import { AdivinanzaComponent } from './pages/adivinanza/adivinanza.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'clima', component: ClimaComponent },
   { path: 'github', component: GithubComponent },
   { path: 'dogs', component: DogsComponent },
-  { path: 'server', component: UsuariosComponent },
   { path: 'calculadora', component: CalculadoraComponent },
+  { path: 'adivinanza', component: AdivinanzaComponent },
   { path: 'camera', component: CameraComponent },
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
--- src/app/pages/calculadora/calculadora.component.ts
+++ src/app/pages/calculadora/calculadora.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'calculator-root',
+  selector: 'calculadora-root',
   templateUrl: './calculadora.component.html',
   styleUrls: ['./calculadora.component.css']
 })
